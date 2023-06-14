54ffc8236257334c054921e45c94b247c361f6a3
Zoya Afreen <zoyafreen04@gmail.com>
2023-06-14 17:51:33 +0530

    Initial Commit

---
+++ I am Riich/I_am_RiichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RiichApp.swift
+//  I am Riich
+//
+//  Created by Zoya Afreen on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct I_am_RiichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
